c4e516dba97ec31290d0775f1113e47b7471bbfd
Melanie Rodriguez <melarodrigg@gmail.com>
2026-04-20 19:44:28 -0600

    Completed walkthrough of technical lesson

--- lib/serialize.py
+++ lib/serialize.py
@@ -1,5 +1,5 @@
 # lib/serialize.py
-
+from marshmallow import Schema, fields
 from pprint import pprint
 
 # model
@@ -9,8 +9,41 @@ class Dog:
         self.name = name
         self.breed = breed
         self.tail_wagging = tail_wagging
+        
+    def give_treat(self):
+        self.tail_wagging = True
+        
+    def scold(self):
+        self.tail_wagging = False
+    
+# schema. Fields can be omitted if needed, for example private info
+class DogSchema(Schema):
+    name = fields.String()
+    breed = fields.String()
+    tail_wagging = fields.Boolean()
 
-# create model instance
+# dog = Dog(name="Snuggles", breed="Beagle", tail_wagging=True)
 
-dog = Dog(name="Snuggles", breed="Beagle", tail_wagging=True)
-print(dog)
+dogs = [
+    Dog(name="Snuggles", breed="Beagle", tail_wagging=True),
+    Dog(name="Wags", breed="Collie", tail_wagging=False)
+]
+
+pprint(DogSchema(many=True).dumps(dogs))
+
+# Fields can be limited during instance too, using "only" parameter. 
+# Default is Schema()
+
+# dog_schema = DogSchema(only=("name", "breed"))
+
+# Fields can be excluded.
+# Expects a list of strings, make sure to include a comma
+
+# dog_schema = DogSchema(exclude=("breed",))
+
+# dumps() can be called upon initialization like so:
+# dog_dict = DogSchema(only=("breed","name")).dumps()
+
+# dog_dict = dog_schema.dumps(dog)
+# pprint(dog_dict)
+
